909fcc77bc718eefc2ab0e6f0ea1854091e8f4f6
Philip Eschenbacher <philip@eschenbacher.ch>
2024-02-13 20:03:30 +0100

    first running version

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RealmAppProvider, useRealmApp } from "./RealmApp";
+import Login from "./Pages/Login";
+import RagApp from "./Pages/RagApp";
 
-function App() {
-  const [count, setCount] = useState(0)
+const APP_ID = import.meta.env.VITE_REALM_APP_ID;
 
+const RequireLoggedInUser = ({ children }) => {
+  const app = useRealmApp();
+  return app.currentUser ? children : <Login />;
+};
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <RealmAppProvider appId={APP_ID}>
+      <RequireLoggedInUser>
+        <RagApp />
+      </RequireLoggedInUser>
+    </RealmAppProvider>
+    
+  );
 }
 
-export default App
+export default App;
